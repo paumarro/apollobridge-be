704339e370988c9d959fc0332dc80d3a9193aa1d
Pau Marro <paumarro98@gmail.com>
2025-03-19 12:59:27 +0100

    Correct DB variable

--- pkg/loadEnvVariables.go
+++ pkg/loadEnvVariables.go
@@ -8,11 +8,11 @@ import (
 func LoadEnvVariables() {
 	// Access and log environment variables
 	port := os.Getenv("PORT")
-	artDbURL := os.Getenv("ART_DB_URL")
+	artDbURL := os.Getenv("PG_DB_URL")
 	keycloakURL := os.Getenv("KEYCLOAK_URL")
 
 	log.Printf("PORT: %s", port)
-	log.Printf("ART_DB_URL: %s", artDbURL)
+	log.Printf("PG_DB_URL: %s", artDbURL)
 	log.Printf("KEYCLOAK_URL: %s", keycloakURL)
 
 	// Example of error handling for critical variables
