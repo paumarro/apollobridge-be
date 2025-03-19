52c7b58aab02bd6cb66fd6fdce2a8faf9737d7c5
Pau Marro <paumarro98@gmail.com>
2025-03-19 13:05:19 +0100

    Refactor DB env variable

--- pkg/loadEnvVariables.go
+++ pkg/loadEnvVariables.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -8,8 +9,14 @@ import (
 func LoadEnvVariables() {
 	// Access and log environment variables
 	port := os.Getenv("PORT")
-	artDbURL := os.Getenv("PG_DB_URL")
 	keycloakURL := os.Getenv("KEYCLOAK_URL")
+	artDbURL := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s",
+		os.Getenv("PGUSER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("RAILWAY_TCP_PROXY_DOMAIN"),
+		os.Getenv("RAILWAY_TCP_PROXY_PORT"),
+		os.Getenv("PGDATABASE"),
+	)
 
 	log.Printf("PORT: %s", port)
 	log.Printf("PG_DB_URL: %s", artDbURL)
@@ -22,4 +29,11 @@ func LoadEnvVariables() {
 
 	// Log the successful loading of environment variables
 	log.Println("Environment variables loaded successfully!")
+
+	log.Printf("PGUSER: %s", os.Getenv("PGUSER"))
+	log.Printf("POSTGRES_PASSWORD: %s", os.Getenv("POSTGRES_PASSWORD"))
+	log.Printf("RAILWAY_TCP_PROXY_DOMAIN: %s", os.Getenv("RAILWAY_TCP_PROXY_DOMAIN"))
+	log.Printf("RAILWAY_TCP_PROXY_PORT: %s", os.Getenv("RAILWAY_TCP_PROXY_PORT"))
+	log.Printf("PGDATABASE: %s", os.Getenv("PGDATABASE"))
+
 }
