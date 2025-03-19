90399b23fcacbccd6048536e185d05e0af0e9a0d
Pau Marro <paumarro98@gmail.com>
2025-03-19 13:15:57 +0100

    log test

--- pkg/loadEnvVariables.go
+++ pkg/loadEnvVariables.go
@@ -10,10 +10,12 @@ func LoadEnvVariables() {
 	port := os.Getenv("PORT")
 	keycloakURL := os.Getenv("KEYCLOAK_URL")
 	artDbURL := os.Getenv("PG_DB_URL")
+	keycloakSecrete := os.Getenv("KEYCLOAK_CLIENT_SECRET")
 
 	log.Printf("PORT: %s", port)
 	log.Printf("PG_DB_URL: %s", artDbURL)
 	log.Printf("KEYCLOAK_URL: %s", keycloakURL)
+	log.Printf("KEYCLOAK_SECRET: %s", keycloakSecrete)
 
 	// Example of error handling for critical variables
 	if port == "" || artDbURL == "" || keycloakURL == "" {
